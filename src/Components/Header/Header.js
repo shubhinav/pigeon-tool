45e90012b2eb12257570f7a8db9892d9b798c1e5
Shubhinav Dua <shubhinavdua@gmail.com>
2022-01-18 17:42:25 +0530

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Icon } from '@iconify/react';
+
+export default function Header() {
+    return (
+        <header className="px-4 py-3 main-header d-flex justify-content-between align-items-center" style={{backgroundColor: "#D8D8D8"}}>
+            <h3>LOGO</h3>
+            <h3>ToolName</h3>
+            <div className="btn-group">
+                <button type="button" style={{background: "transparent"}} className="btn d-flex justify-content-between align-items-center dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <Icon icon="ant-design:user-outlined" /> 
+                    <span className="mx-2">John Doe</span>
+                </button>
+                <div className="dropdown-menu dropdown-menu-right">
+                    <button className="dropdown-item" type="button">Change Password</button>
+                    <button className="dropdown-item" type="button">Log Out</button>
+                </div>
+            </div>
+        </header>
+    )
+}
+
