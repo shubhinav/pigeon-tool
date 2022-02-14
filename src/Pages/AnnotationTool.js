80755eff66e6b1217548ed645a1fa07f4bf211c8
Shubhinav Dua <shubhinavdua@gmail.com>
2022-02-14 21:12:43 +0530

    iframe testing 2

--- src/Pages/AnnotationTool.js
+++ src/Pages/AnnotationTool.js
@@ -2,6 +2,6 @@ import React from "react"
 
 export default function AnnotationTool(){
     return(
-        <iframe src="http://localhost:8080/tasks/2/jobs/2" title="W3Schools Free Online Web Tutorials"></iframe>
+        <iframe src="http://localhost:8080/tasks/2/jobs/2" width="100%" height="100%" title="W3Schools Free Online Web Tutorials"></iframe>
     )
 }
